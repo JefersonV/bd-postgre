f22b7fc14c4d59b9332b6d24dfe1e32731be1127
Donald <thomaschavajay20@gmail.com>
2022-09-15 19:02:51 -0600

    "Tablas con relaciones terminada"

--- script-sistema.sql
+++ script-sistema.sql
@@ -94,6 +94,98 @@ CREATE TABLE tipo_materia_prima(
 );
 
 --Tablas con relaciones con las tablas anteriores
+--Tabla de Producto
+CREATE TABLE producto(
+    id_producto SERIAL PRIMARY KEY,
+    nombre VARCHAR(45),
+    stock_ingreso INT, 
+    id_unidad_medida INT,
+    tipo_producto INT,
+    stock_actual INT,
+    precio_venta DOUBLE PRECISION,
+    stock_minimo INT,
+    id_costo_produccion INT,
+    FOREIGN KEY (id_unidad_medida) REFERENCES unidad_de_medida(id_unidad_medida),
+    FOREIGN KEY (tipo_producto) REFERENCES tipo_producto(id_tipo_producto),
+    FOREIGN KEY (id_costo_produccion) REFERENCES costo_produccion(id_costo_produccion)
+);
+
+--Tabla de Compra
+CREATE TABLE compras(
+    id_compra SERIAL PRIMARY KEY,
+    fecha DATE,
+    cantidad INT,
+    precio_unitario DOUBLE PRECISION,
+    descuento DOUBLE PRECISION,
+    subtotal DOUBLE PRECISION,
+    total DOUBLE PRECISION,
+    no_comprobante INT,
+    observaciones VARCHAR(45),
+    id_tipo_comprobante INT, 
+    id_proveedor INT, 
+    id_producto INT, 
+    id_modo_pago INT,
+    id_usuario INT,
+    FOREIGN KEY (id_tipo_comprobante) REFERENCES tipo_comprobante(id_tipo_comprobante),
+    FOREIGN KEY (id_proveedor) REFERENCES proveedor(id_proveedor),
+    FOREIGN KEY (id_producto) REFERENCES producto(id_producto),
+    FOREIGN KEY (id_modo_pago) REFERENCES modo_pago(id_modo_pago),
+    FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
+);
+
+CREATE TABLE venta(
+    id_venta SERIAL PRIMARY KEY,
+    fecha DATE,
+    cantidad INT, 
+    descripcion VARCHAR(45),
+    descuento DOUBLE PRECISION,
+    subtotal DOUBLE PRECISION,
+    total DOUBLE PRECISION,
+    no_factura INT,
+    id_cliente INT,
+    id_producto INT,
+    id_modo_pago INT,
+    id_usuario INT,
+    FOREIGN KEY (no_factura) REFERENCES factura(no_factura),
+    FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente),
+    FOREIGN KEY (id_producto) REFERENCES producto(id_producto),
+    FOREIGN KEY (id_modo_pago) REFERENCES modo_pago(id_modo_pago),
+    FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
+);
+
+--Tabla pedido cliente
+CREATE TABLE pedido_cliente(
+    id_pedido_cliente INT,
+    fecha DATE,
+    cantidad_producto VARCHAR(45),
+    descuento DOUBLE PRECISION,
+    total DOUBLE PRECISION,
+    id_cliente INT,
+    id_usuario INT,
+    id_producto INT,
+    FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente),
+    FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
+    FOREIGN KEY (id_producto) REFERENCES producto(id_producto)
+);
+
+--Inventario_movimientos
+CREATE TABLE inventario_movimiento(
+    fecha DATE, --Tengo dudas en esta, por que en el diagrama es Date time y es primary key
+    tipo_operacion VARCHAR(45),
+    decuento DOUBLE PRECISION,
+    total_operacion DOUBLE PRECISION,
+    id_usuario INT,
+    id_cliente INT, 
+    id_proveedor INT,
+    id_producto INT,
+    id_modo_pago INT,
+    FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
+    FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente),
+    FOREIGN KEY (id_proveedor) REFERENCES proveedor(id_proveedor),
+    FOREIGN KEY (id_producto) REFERENCES producto(id_producto),
+    FOREIGN KEY (id_modo_pago) REFERENCES modo_pago(id_modo_pago)
+);
+
 --Tabla operacion
 CREATE TABLE operacion(
 	id_operacion SERIAL PRIMARY KEY,
@@ -137,19 +229,52 @@ CREATE TABLE servicio_cafe(
 	FOREIGN KEY (id_unidad_medida) REFERENCES unidad_de_medida(id_unidad_medida)
 );
 
---Tabla costo producción
-CREATE TABLE costo_produccion(
-	id_costo_produccion SERIAL PRIMARY KEY,
-	cantidad INT,
-	precio_venta DOUBLE PRECISION,
-	costo_por_libra DOUBLE PRECISION,
-	ganancia_neta DOUBLE PRECISION,
-	id_empaque INT,
-	id_tipo_materia INT,
-	id_unidad_medida INT,
-	id_servicio_cafe INT,
-	FOREIGN KEY (id_empaque) REFERENCES tipo_empaque(id_empaque),
-	FOREIGN KEY (id_tipo_materia) REFERENCES tipo_materia_prima(id_tipo_materia)
-	FOREIGN KEY (id_unidad_medida) REFERENCES unidad_de_medida(id_unidad_medida)
-	FOREIGN KEY (id_servicio_cafe) REFERENCES servicio_cafe(id_servicio_cafe)
+--Devolución proveedor
+CREATE TABLE devolucion_proveedor(
+    fecha DATE,
+    detalle_devolucion VARCHAR(45),
+    sub_total DOUBLE PRECISION,
+    total DOUBLE PRECISION,
+    id_transportista INT, 
+    id_compra INT,
+    FOREIGN KEY (id_transportista) REFERENCES transportista(id_transportista),
+    FOREIGN KEY (id_compra) REFERENCES compras(id_compra)
+);
+
+--Devolucion cliente
+CREATE TABLE devolucion_cliente(
+    id_dev_cliente INT,
+    fecha DATE,
+    detalle_devolucion VARCHAR(45),
+    sub_total DOUBLE PRECISION,
+    total DOUBLE PRECISION,
+    id_venta INT,
+    id_transportista INT, 
+    id_cliente INT,
+    id_usuario INT, 
+    FOREIGN KEY(id_venta) REFERENCES venta(id_venta),
+    FOREIGN KEY(id_transportista) REFERENCES transportista(id_transportista),
+    FOREIGN KEY(id_cliente) REFERENCES cliente(id_cliente),
+    FOREIGN KEY(id_usuario) REFERENCES usuario(id_usuario)
+);
+
+--Materia Prima
+CREATE TABLE materia_prima(
+    id_materia_prima SERIAL PRIMARY KEY,
+    cantidad DOUBLE PRECISION,
+    costo DOUBLE PRECISION,
+    id_unidad_medida INT,
+    id_tipo_materia INT,
+    fecha DATE,
+    FOREIGN KEY(id_unidad_medida) REFERENCES unidad_de_medida(id_unidad_medida),
+    FOREIGN KEY(id_tipo_materia) REFERENCES tipo_materia_prima(id_tipo_materia)
+);
+
+--Tabla Material de Empaque
+CREATE TABLE material_empaque(
+    id_empaque SERIAL PRIMARY KEY,
+    fecha DATE,
+    costo DOUBLE PRECISION,
+    id_tipo_empaque INT,
+    FOREIGN KEY (id_tipo_empaque) REFERENCES tipo_empaque(id_tipo_empaque)
 );
