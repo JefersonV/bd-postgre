3e43b6a2f5da421c6c3596fa677d56572ab44ce6
Donald <thomaschavajay20@gmail.com>
2022-10-27 11:39:21 -0600

    "Cambios en el trigger"

--- script-triggers.sql
+++ script-triggers.sql
@@ -17,3 +17,40 @@ LANGUAGE plpgsql;
 CREATE TRIGGER TR_UPDATE_STOCK_PRODUCTO AFTER INSERT ON venta
 FOR EACH ROW
 EXECUTE PROCEDURE SP_TR_INSERT_VENTA_UPDATE_STOCK_PRODUCT();
+
+-- ------------------------------------------------------------
+CREATE FUNCTION sp_tr_inser_venta_insert_log_mov_inventario() RETURNS TRIGGER
+AS
+$$
+BEGIN
+INSERT INTO inventario_movimiento(fecha, tipo_operacion, descuento, total_operacion, id_venta) 
+VALUES (new.fecha, 'Venta', new.descuento, new.total, new.id_venta);
+RETURN NEW;
+END
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER tr_insert_log_mov_inventario AFTER INSERT ON venta
+FOR EACH ROW
+EXECUTE PROCEDURE sp_tr_inser_venta_insert_log_mov_inventario();
+
+
+-- Trigger para dejar contancia en movimiento inventario de la compra
+CREATE FUNCTION sp_tr_inser_compra_insert_log_mov_inventario() RETURNS TRIGGER
+AS
+$$
+BEGIN
+INSERT INTO inventario_movimiento(fecha, tipo_operacion, descuento, total_operacion, id_compra) 
+VALUES (new.fecha, 'Compra', new.descuento, new.total, new.id_compra);
+RETURN NEW;
+END
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER tr2_insert_log_mov_inventario AFTER INSERT ON compras
+FOR EACH ROW
+EXECUTE PROCEDURE sp_tr_inser_compra_insert_log_mov_inventario();
+
+INSERT INTO venta(fecha, cantidad, descripcion, descuento, subtotal, 
+				  total, id_factura, id_producto, id_modo_pago)
+VALUES (CURRENT_DATE, 10, 'Prueba para Log', 0, 25, 25, 1, 1, 1);
